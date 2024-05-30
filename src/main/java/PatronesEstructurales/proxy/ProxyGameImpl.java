2c9cc6e9334cc28270a08a4b6920a3a0e1fc6f45
111858-DelfinoDavid <111858@tecnicatura.frc.utn.edu.ar>
2024-05-30 16:29:03 -0300

    Proxy
    Patrón Estructural Proxy: consiste en tener una copia de un objeto cuyo acceso es complicado o restringido y esa dificultad de acceso puede ser por varios motivos:
    
    - Proxy de protección: tengo un objeto de uso y de acceso restringido. Lo que hacemos es obtener una copia de ese objeto y trabajar sobre ella y a la hora de guardar o actualizar es cuando se trendrán en cuenta las restricciones de seguridad.
    
    -Proxy Remoto: hay un objeto que está en remoto y el acceso es complicado. Obtenemos el objeto en un request, hacemos una copia y trabajamos sobre ella. Al momento de hacer un commit o actualizar es cuando se tendrán en cuenta las restricciones de seguridad.
    
    -Proxy Virtual: es un objeto que esta guardado en disco y que necesitamos contantemente estar accediendo a  él, escribirlo y modificarlo. Lo que se hace es crear una copia de ese archivo, trabajamos sobre ella y cuando guardamos llamamos al almacenamiento físico. Ej.: una base de datos en JPA.

---
+++ src/main/java/PatronesEstructurales/proxy/ProxyGameImpl.java
@@ -0,0 +1,44 @@
+package PatronesEstructurales.proxy;
+
+import java.util.Date;
+
+public class ProxyGameImpl implements Game {
+    private Stats stats;
+    private GameImpl gameImpl;
+
+    public Stats getStats() {
+        return stats;
+    }
+
+    public ProxyGameImpl setStats(Stats stats) {
+        this.stats = stats;
+        return this;
+    }
+
+    public ProxyGameImpl(GameImpl gameImpl) {
+        this.gameImpl = gameImpl;
+    }
+
+    public GameImpl getGameImpl() {
+        return gameImpl;
+    }
+
+    public ProxyGameImpl setGameImpl(GameImpl gameImpl) {
+        this.gameImpl = gameImpl;
+        return this;
+    }
+
+    @Override
+    public void save(String name) {
+        stats.setLastSave(new Date());
+        gameImpl.setStats(stats);
+        gameImpl.save(name);
+    }
+
+    @Override
+    public Stats load(String name) {
+        Stats stats = getGameImpl().load(name);
+        setStats(stats);
+        return stats;
+    }
+}
